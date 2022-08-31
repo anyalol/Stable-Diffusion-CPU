19d410770c69dfd014aa6421d425cb0dc7b7e592
Sylvain Filoni <sylvain.fillion15@gmail.com>
2022-08-31 11:44:29 +0000

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ model_id = "CompVis/stable-diffusion-v1-4"
 device = "cuda"
 
 #If you are running this code locally, you need to either do a 'huggingface-cli login` or paste your User Access Token from here https://huggingface.co/settings/tokens into the use_auth_token field below. 
-pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True, revision="fp16", torch_dtype=torch.float16)
+pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=False, revision="fp16", torch_dtype=torch.float16)
 pipe = pipe.to(device)
 #When running locally, you won`t have access to this, so you can remove this part
 word_list_dataset = load_dataset("stabilityai/word-list", data_files="list.txt", use_auth_token=True)
