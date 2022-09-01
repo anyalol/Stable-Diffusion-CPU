b0898d13f19510f8661c70ca9c853029d43358ac
Sylvain Filoni <sylvain.fillion15@gmail.com>
2022-09-01 08:37:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from torch import autocast
 from diffusers import StableDiffusionPipeline
 
 YOUR_TOKEN="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"
-
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN, revision="fp16", torch_dtype=torch.float16)
-pipe.to("cuda")
+pipe.to(device)
 
 prompt = "a photograph of an astronaut riding a horse"
 
