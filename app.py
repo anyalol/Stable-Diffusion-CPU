fe7b0434b36a19a390c11cf8025997d673e0b8b1
Sylvain Filoni <sylvain.fillion15@gmail.com>
2022-09-01 08:46:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from torch import autocast
 from diffusers import StableDiffusionPipeline
 
 YOUR_TOKEN="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"
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN, revision="fp16", torch_dtype=torch.float16)
+device = "cpu"
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN)
 pipe.to(device)
 
 
