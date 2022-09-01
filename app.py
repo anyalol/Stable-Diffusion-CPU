8c8ecdc32e3f54acb230e182ee815aa9a73828ff
Sylvain Filoni <sylvain.fillion15@gmail.com>
2022-09-01 09:11:59 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import gradio as gr
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+
+print("hello sylvain")
+
+YOUR_TOKEN="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"
+
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN)
+pipe.to("cuda")
 
 def process(name):
   return "hello " + name
