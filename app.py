b8e38c27a604887373cc6fa1afc8414a2f7f47ac
Sylvain Filoni <sylvain.fillion15@gmail.com>
2022-09-01 08:31:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from diffusers import StableDiffusionPipeline
 
 YOUR_TOKEN="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"
 
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN)
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN, revision="fp16", torch_dtype=torch.float16)
 pipe.to("cuda")
 
 prompt = "a photograph of an astronaut riding a horse"
