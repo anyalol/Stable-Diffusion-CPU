3a7b25b276ad0a69c22590359cd0072e5e8ff4dd
Sylvain Filoni <sylvain.fillion15@gmail.com>
2022-09-01 15:23:01 +0000

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,10 @@ pipe.to(device)
 
 gallery = gr.Gallery(label="Generated images", show_label=False, elem_id="gallery").style(grid=[2], height="auto")
 
-def infer(prompt):
-    
+def infer(prompt): 
     
     #image = pipe(prompt, init_image=init_image)["sample"][0]
-    images_list = pipe([prompt] * 1)
+    images_list = pipe([prompt] * 4)
     images = []
     safe_image = Image.open(r"unsafe.png")
     for i, image in enumerate(images_list["sample"]):
@@ -33,12 +32,13 @@ def infer(prompt):
             images.append(safe_image)
         else:
             images.append(image)
+    
     return images
 
 print("Great sylvain ! Everything is working fine !")
 
 title="Stable Diffusion CPU"
-description="Stable Diffusion example using CPU and HF token. Warning: Slow process... ~5/10 min inference time" 
+description="Stable Diffusion example using CPU and HF token. <br />Warning: Slow process... ~5/10 min inference time. <b>NSFW filter enabled.</b>" 
 
 gr.Interface(fn=infer, inputs="text", outputs=gallery,title=title,description=description).launch(enable_queue=True)
 
