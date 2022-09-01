162116e89ff93580720406103e66f9479222d534
Sylvain Filoni <sylvain.fillion15@gmail.com>
2022-09-01 09:01:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import gradio as gr
-import torch
-from diffusers import StableDiffusionPipeline
 
-YOUR_TOKEN="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"
-device = "cpu"
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN)
-pipe.to(device)
 
-def process :
-  return "hello"
+
+def process(name) :
+  return "hello " + name
   
 gr.Interface(fn=process, inputs="text", outputs="text").launch()
