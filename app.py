fe77bfabdc3345b3c35c53ecd589b06ade538814
Sylvain Filoni <sylvain.fillion15@gmail.com>
2022-09-01 08:21:53 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 import gradio as gr
-
 import torch
-from torch import autocast
+
 from diffusers import StableDiffusionPipeline
-from datasets import load_dataset
-from PIL import Image  
-import re
 
 YOUR_TOKEN="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"
 
-pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16, use_auth_token=YOUR_TOKEN)
+pipe = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4", use_auth_token=YOUR_TOKEN)
+pipe.to("cuda")
 
 prompt = "a photograph of an astronaut riding a horse"
 
